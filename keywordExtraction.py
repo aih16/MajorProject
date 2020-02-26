40c54f6ba01da37ae529bcaa990321fe255e1436
Aidan <aih16@aber.ac.uk>
2020-02-26 16:45:42 +0000

    Extract keywords from file
    keywordExtraction now pulls from file, creates ranked list from keywords and then stores in file for later Sentiment Analysis

--- keywordExtraction.py
+++ keywordExtraction.py
@@ -4,8 +4,12 @@ from rake_nltk import Rake
 def keywordExtraction():
     r = Rake()
 
-    # Extracts keywords and ranks them
-    r.extract_keywords_from_text("Keyword extraction is not that difficult after all. There are many libraries that can help you with keyword extraction. Rapid automatic keyword extraction is one of those.")
+    # Opens file, reads data and stores as string 'data'
+    with open("extractThis.txt", 'r') as file:
+        data = file.read().replace('\n', '')
+
+    # Extracts keywords from data file and ranks them
+    r.extract_keywords_from_text(data)
 
     # Creates list of ranked keywords
     ranked_phrases = r.get_ranked_phrases()
@@ -13,7 +17,7 @@ def keywordExtraction():
     # Creates file named ranked_phrases and adds the ranked keywords
     text_file = open("ranked_phrases.txt", "w")
     for ele in ranked_phrases:
-        text_file.write(ele+'\n')
+        text_file.write(ele + '\n')
     text_file.close()
 
     print("Phrases added to file.")
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ access_token = "3011525667-NPo9lLm3fFilJjyUYO6Xr5KiL5QDoZxV5daLTTq"
 access_token_secret = "8t91piYB8hXXvCwSmAT7FMdY9pPoOxRy6LFkAsDa1sK50"
 
 if __name__ == "__main__":
-    pullTweets()
+    #pullTweets()
     keywordExtraction()
 
 print("End")
