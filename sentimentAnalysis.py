542be7af17f98195a3eedb07352638ce14529aa2
Aidan <aidan2ed@hotmail.co.uk>
2020-03-17 12:06:21 +0000

    SA trained and working
    SA (NaiveBayers) has been provided training and test data.

--- sentimentAnalysis.py
+++ sentimentAnalysis.py
@@ -1,46 +1,41 @@
 from textblob import TextBlob
 from textblob import classifiers
+from textblob.classifiers import NaiveBayesClassifier
 
-# POS = Part-of-speech, NN = Noun, DT = Determiner etc
+
 def SA():
     trainingData = [
-        ('Tom Holland is a terrible spiderman.', 'pos'),
-        ('a terrible Javert (Russell Crowe) ruined Les Miserables for me...', 'pos'),
-        ('The Dark Knight Rises is the greatest superhero movie ever!', 'neg'),
-        ('Fantastic Four should have never been made.', 'pos'),
-        ('Wes Anderson is my favorite director!', 'neg'),
-        ('Captain America 2 is pretty awesome.', 'neg'),
-        ('Let\s pretend "Batman and Robin" never happened..', 'pos'),
+        ('Tom Holland is a terrible spiderman.', 'neg'),
+        ('a terrible Javert (Russell Crowe) ruined Les Miserables for me...', 'neg'),
+        ('The Dark Knight Rises is the greatest superhero movie ever!', 'pos'),
+        ('Fantastic Four should have never been made.', 'neg'),
+        ('Wes Anderson is my favorite director!', 'pos'),
+        ('Captain America 2 is pretty awesome.', 'pos'),
+        ('Lets pretend "Batman and Robin" never happened..', 'neg'),
+        ('I love this sandwich.', 'pos'),
+        ('this is an amazing place!', 'pos'),
+        ('I feel very good about these beers.', 'pos'),
+        ('this is my best work.', 'pos'),
+        ("what an awesome view", 'pos'),
+        ('I do not like this restaurant', 'neg'),
+        ('I am tired of this stuff.', 'neg'),
+        ("I can't deal with this", 'neg'),
+        ('he is my sworn enemy!', 'neg'),
+        ('my boss is horrible.', 'neg')
     ]
     testingData = [
-        ('Superman was never an interesting character.', 'pos'),
-        ('Fantastic Mr Fox is an awesome film!', 'neg'),
-        ('Dragonball Evolution is simply terrible!!', 'pos')
+        ('Superman was never an interesting character.', 'neg'),
+        ('Fantastic Mr Fox is an awesome film!', 'pos'),
+        ('Dragonball Evolution is simply terrible!!', 'neg'),
+        ('the beer was good.', 'pos'),
+        ('I do not enjoy my job', 'neg'),
+        ("I ain't feeling dandy today.", 'neg'),
+        ("I feel amazing!", 'pos'),
+        ('Gary is a friend of mine.', 'pos'),
+        ("I can't believe I'm doing this.", 'neg')
     ]
 
-    classifier = classifiers.NaiveBayesClassifier(trainingData)
-
-    print(classifier.accuracy(testingData))
-    classifier.show_informative_features(10)
-
-    blob = TextBlob('the weather is terrible!', classifier=classifier)
-    print(blob.classify())
-
-    """if trainingData.sentiment.polarity > 0:
-        print("Data sentiment is positive with a sentiment polarity of", trainingData.sentiment.polarity)
-    elif trainingData.sentiment.polarity < 0:
-        print("Data sentiment is negative with a sentiment polarity of", trainingData.sentiment.polarity)
-    else:
-        print("Data sentiment is neutral with a sentiment polarity of", trainingData.sentiment.polarity)
-
-    for np in trainingData.noun_phrases:
-            print(np)
-
-        for words, tag in trainingData.tags:
-            print(words, tag)
-
-        print(trainingData.sentences[1].words[1])
-        print(trainingData.sentences[1].words[1].singularize())
-
-        for ngram in trainingData.ngrams(2):
-            print(ngram)"""
+    cl = NaiveBayesClassifier(trainingData)
+    cl.show_informative_features(10)
+    DemoInput = input("Please provide a test input: ")
+    print(cl.classify(DemoInput))
